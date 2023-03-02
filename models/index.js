e1be6fea42a90951a479a4278c851eabffa15a85
dianaguu <dianaguu@gmail.com>
2023-03-02 21:39:39 +0800

    feat: add user model

--- models/index.js
+++ models/index.js
@@ -1,21 +1,22 @@
-'use strict';
+'use strict'
 
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-const process = require('process');
-const basename = path.basename(__filename);
-const env = process.env.NODE_ENV || 'development';
-const config = require(__dirname + '/../config/config.json')[env];
-const db = {};
+const fs = require('fs')
+const path = require('path')
+const Sequelize = require('sequelize')
+const process = require('process')
+const basename = path.basename(__filename)
+const env = process.env.NODE_ENV || 'development'
+const config = require(path.resolve(__dirname, '../config/config.json'))[env]
+const db = {}
 
-let sequelize;
+// connect database
+let sequelize
 if (config.use_env_variable) {
-  sequelize = new Sequelize(process.env[config.use_env_variable], config);
+  sequelize = new Sequelize(process.env[config.use_env_variable], config)
 } else {
-  sequelize = new Sequelize(config.database, config.username, config.password, config);
+  sequelize = new Sequelize(config.database, config.username, config.password, config)
 }
-
+// import models into db
 fs
   .readdirSync(__dirname)
   .filter(file => {
@@ -24,20 +25,21 @@ fs
       file !== basename &&
       file.slice(-3) === '.js' &&
       file.indexOf('.test.js') === -1
-    );
+    )
   })
   .forEach(file => {
-    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes);
-    db[model.name] = model;
-  });
-
+    const model = require(path.join(__dirname, file))(sequelize, Sequelize.DataTypes)
+    db[model.name] = model
+  })
+// create associations between models
 Object.keys(db).forEach(modelName => {
+  console.log(db[modelName].associate)
   if (db[modelName].associate) {
-    db[modelName].associate(db);
+    db[modelName].associate(db)
   }
-});
+})
+// exports sequelize connection and Object
+db.sequelize = sequelize
+db.Sequelize = Sequelize
 
-db.sequelize = sequelize;
-db.Sequelize = Sequelize;
-
-module.exports = db;
+module.exports = db
