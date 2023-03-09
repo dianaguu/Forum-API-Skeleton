5904685b7a4e550a50ff0d5de76f00a83e48f5f0
dianaguu <dianaguu@gmail.com>
2023-03-09 16:02:37 +0800

    feat: add objective model

---
+++ models/objective.js
@@ -0,0 +1,29 @@
+'use strict'
+const {
+  Model
+} = require('sequelize')
+module.exports = (sequelize, DataTypes) => {
+  class Objective extends Model {
+    /**
+     * Helper method for defining associations.
+     * This method is not a part of Sequelize lifecycle.
+     * The `models/index` file will call this method automatically.
+     */
+    static associate (models) {
+      // define association here
+    }
+  }
+  Objective.init({
+    name: DataTypes.STRING,
+    telephone: DataTypes.STRING,
+    address: DataTypes.STRING,
+    openingHours: DataTypes.STRING,
+    description: DataTypes.TEXT
+  }, {
+    sequelize,
+    modelName: 'Objective',
+    tableName: 'Objectives',
+    underscored: true
+  })
+  return Objective
+}
