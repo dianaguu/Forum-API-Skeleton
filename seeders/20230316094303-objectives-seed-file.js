2ed7a9301127bf6727fbe7dc59de4c7fb000f755
dianaguu <dianaguu@gmail.com>
2023-03-16 19:09:41 +0800

    feat: add seed data for categories

--- seeders/20230316094303-objectives-seed-file.js
+++ seeders/20230316094303-objectives-seed-file.js
@@ -4,6 +4,10 @@ const { faker } = require('@faker-js/faker')
 
 module.exports = {
   up: async (queryInterface, Sequelize) => {
+    const categories = await queryInterface.sequelize.query(
+      'SELECT id FROM Categories;',
+      { type: queryInterface.sequelize.QueryTypes.SELECT }
+    )
     await queryInterface.bulkInsert('Objectives',
       Array.from({ length: 50 }, () => ({
         name: faker.company.name(),
@@ -13,7 +17,8 @@ module.exports = {
         image: `https://loremflickr.com/320/240/restaurant,food/?random=${Math.random() * 100}`,
         description: faker.lorem.text(),
         created_at: new Date(),
-        updated_at: new Date()
+        updated_at: new Date(),
+        category_id: categories[Math.floor(Math.random() * categories.length)].id
       }))
     )
   },
